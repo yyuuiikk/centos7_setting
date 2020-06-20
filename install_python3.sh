e0e3c8f1af87824e14bbbe0400c509f6596d2850
yyuuiikk <yutaikawa0315@gmail.com>
2020-06-20 15:33:58 +0900

    ius-release.npmのアドレスが変更されていたため修正

--- install_python3.sh
+++ install_python3.sh
@@ -1,4 +1,4 @@
-sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
 sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
 
 # pythonライブラリインストール
